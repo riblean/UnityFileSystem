eee4051d0df34a9693a0c999a58907db85fa64cc
riblean <78409097+riblean@users.noreply.github.com>
2021-02-02 17:26:42 +0900

    Add files via upload

---
+++ DataManager.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ファイル保存関係。2021年2月2日
+public class DataManager
+{
+    static DataManager instace;
+    string filePath = "";
+
+    public static DataManager Instance
+    {
+        get
+        {
+            if(instace == null){ instace = new DataManager(); }
+            return instace;
+        }
+    }
+
+    DataManager()
+    {
+        filePath = Application.dataPath + "/.SaveData/";
+        if(!Directory.Exists(filePath))
+        {
+            Directory.CreateDirectory(filePath);
+            Debug.Log("DataManager : .SaveDataフォルダを新規作成しました" + filePath);
+        }
+    }
+
+    public bool IsFile(string _str)
+    {   // ファイル/ディレクトリがが存在するか？
+        return File.Exists(filePath + _str);
+    }
+
+    public void CreateDirectory(string _str)
+    {
+        Directory.CreateDirectory(filePath + _str);
+    }
+
+    public string[] FileList()
+    {   // フォルダ名を取得。
+        string[] _strs = Directory.GetFiles(filePath, "*");
+        for(int i = 0; i < _strs.Length; i++)
+        {
+            _strs[i] = Path.GetFileNameWithoutExtension(_strs[i]);
+        }
+        return _strs;
+    }
+
+    // csv
+    public bool SaveCSV(string[][] _contens, string _fileName = "temp.csv")
+    {
+        // if(!IsFile(_fileName)){return false;}
+        StreamWriter _steram = new StreamWriter(filePath + _fileName, false, Encoding.GetEncoding("Shift_JIS"));
+        for(int i = 0; i < _contens.Length; i++)
+        {
+            string _str = string.Join(",", _contens[i]);
+            _steram.WriteLine(_str);
+        }
+        _steram.Close();
+        return true;
+    }
+
+    public bool LoadCSV(ref string[][] _content, string _fileName = "temp.csv")
+    {
+        if(IsFile(_fileName))
+        {
+            StreamReader _stream = new StreamReader(filePath + _fileName);
+            string[] _dataLines = _stream.ReadToEnd().Replace("\r\n", "\n").Split(new[]{'\n', '\r'});
+            _stream.Close();
+
+            _content = new string[_dataLines.Length][];
+            for (int i = 0; i < _dataLines.Length; i++)
+            {
+                _content[i] = _dataLines[i].Split(',');
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public bool SaveJson<Type>(Type _type, string _fileName)
+    {
+        string _json = JsonUtility.ToJson(_type);
+        StreamWriter _stream = new StreamWriter(filePath + _fileName, false, Encoding.GetEncoding("Shift_JIS"));
+        _stream.Write(_json);
+        _stream.Flush();
+        _stream.Close();
+        return true;
+    }
+
+    public bool LoadJson<Type>(ref Type _type, string _fileName)
+    {
+        if(IsFile(_fileName))
+        {
+            StreamReader _stream = new StreamReader(filePath + _fileName);
+            string _data = _stream.ReadToEnd();
+            _stream.Close();
+            _type = JsonUtility.FromJson<Type>(_data);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ SettingManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SettingManager : MonoBehaviour
+{
+    const string fileName = "UserData.txt";
+    public static SettingManager Instance{get; private set;}
+    public string[][] _content;
+
+    const string DateTimeFormat = "yyyy/MM/dd/HH:mm:ss";
+    void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            _content = new string[][]{
+                new string[]{"PlayCount", "0"},
+                new string[]{"PlayTime/m", "0"},
+                new string[]{"LastPlay", "2000/01/01/00:00:01"}
+            };
+            DataManager.Instance.LoadCSV(ref _content, fileName);
+
+            float _PlayCount = 0;
+            float.TryParse(_content[0][1], out _PlayCount);
+            _PlayCount++;
+            _content[0][1] = _PlayCount.ToString();
+            DateTime _dt = DateTime.Now;
+            _content[2] = new string[]{"LastPlay", _dt.ToString(DateTimeFormat)};
+
+            Settings.Load();
+            Debug.Log(Settings.Instance.MainVolume);
+        }
+    }
+    
+    public void OnApplicationQuit()
+    {
+        DateTime _dtTarget = DateTime.ParseExact(_content[2][1], DateTimeFormat, null);
+        DateTime _dt = DateTime.Now;
+        _content[2] = new string[]{"LastPlay", _dt.ToString(DateTimeFormat)};
+        TimeSpan _ts = _dt - _dtTarget;
+        float _playTime = 0;
+        float.TryParse(_content[1][1], out _playTime);
+        _content[1] = new string[]{"PlayTime/m", (_playTime + _ts.TotalMinutes).ToString()};
+        DataManager.Instance.SaveCSV(_content, fileName);
+
+        Settings.Save();
+        Debug.Log("QuitSave");
+    }
+}
+
+public class Settings
+{
+    const string fileName = "setting.json";
+
+    public float MainVolume = 0.1f;
+
+    public static Settings Instance;
+    
+    static public void Save()
+    {
+        DataManager.Instance.SaveJson<Settings>(Instance, fileName);
+    }
+
+    static public void Load()
+    {
+        Instance = new Settings();
+        DataManager.Instance.LoadJson<Settings>(ref Instance, fileName);
+    }
+}
